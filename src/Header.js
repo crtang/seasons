65fbc4c9f2628710e5800f20850e7d8ad08737ea
ctang <11547113+crtang@users.noreply.github.com>
2024-07-23 15:13:11 -0700

    Created PageTitle.js and added minimum code; imported it to Header.js and incorporated it into code

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import PageTitle from './PageTitle';
 
 const Header = () => {
   return (
     <header>
       <h1>seasons</h1>
+      <PageTitle />
     </header>
   )
 };
---
+++ src/PageTitle.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const PageTitle = () => {
+  return (
+    <h1>Seasons</h1>
+  );
+};
+
+export default PageTitle;
