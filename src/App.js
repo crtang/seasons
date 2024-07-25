088eae5d91a437de1825616045962eb43d80d63e
ctang <11547113+crtang@users.noreply.github.com>
2024-07-25 10:51:18 -0700

    Put each component in their own subfolders and renamed files accordingly so components are easily reusable

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import Header from "./Header";
+import Header from "./Header/Header";
 import ClockAnalog from './ClockAnalog';
 import ToDoList from './ToDoList';
 
