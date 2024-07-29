5daba31247707bcc68589eb722e510acf9cd83fa
ctang <11547113+crtang@users.noreply.github.com>
2024-07-29 10:52:12 -0700

    Added editThis and deleteThis functions to ListItem component and edited its JSX; also adjusted List component accordingly

--- src/ToDoList/List/ListItem/index.js
+++ src/ToDoList/List/ListItem/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
 
-const ListItem = () => {
+const ListItem = (props) => {
+  const editThis = () => {
+    const taskToEditId = props.task.id;
+    props.editTask(taskToEditId);
+  };
+
+  const deleteThis = () => {
+    const taskToDeleteId = props.task.id;
+    props.deleteTask(taskToDeleteId);
+  };
+
   return (
-    <li></li>
+    <li>
+      <label class="task">
+        <input class="is-task-done" type="checkbox" />
+        <span class="checkmark">{ props.task.item }</span>
+        <span class="task-content"></span>
+        <span class="edit-task" onClick={editThis}></span>
+        <span class="delete-task" onClick={deleteThis}></span>
+      </label>
+    </li>
   );
 };
 
--- src/ToDoList/List/index.js
+++ src/ToDoList/List/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ListItem from './ListItem';
 
 const List = (props) => {
-  const renderList = (task) => {
+  const renderList = (task) =>
     <ListItem 
       task={task}
       key={task.id}
+      deleteTask={props.deleteTask}
     />
-  };
+  ;
 
   const listElements = props.taskList.map(renderList);
 
