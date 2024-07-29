b960bc35539497742c97979496b436ae1629088c
ctang <11547113+crtang@users.noreply.github.com>
2024-07-29 11:16:59 -0700

    Filled in JSX for NewTask component and incorporated it into ListHeader component

--- src/ToDoList/ListHeader/NewTask/index.js
+++ src/ToDoList/ListHeader/NewTask/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 const NewTask = (props) => {
   return (
     <div>
-      <input />
+      <input type="text" placeholder="e.g. buy groceries" /> <button onClick={props.addTask}></button>
     </div>
   );
 };
--- src/ToDoList/ListHeader/index.js
+++ src/ToDoList/ListHeader/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import NewTask from './NewTask';
 
 const ListHeader = (props) => {
   return (
     <div>
       <h2 id="to-do_list_title">to do list</h2>
+      <NewTask />
     </div>
   );
 };
