d0f9019638c43d5509cb9a7c06dfe17d610339c0
ctang <11547113+crtang@users.noreply.github.com>
2024-07-30 10:20:37 -0700

    Filled in functions and JSX for NewTask component

--- src/ToDoList/ListHeader/NewTask/index.js
+++ src/ToDoList/ListHeader/NewTask/index.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const NewTask = (props) => {
+function NewTask (props) {
+  const [taskName, setTaskName] = useState("");
+
+  function changeTask (e) {
+    setTaskName(e.target.value);
+  };
+
+  function addNewTask (e) {
+    e.preventDefault();
+    props.addTask(taskName);
+    setTaskName("");
+  };
+
   return (
-    <div>
-      <input type="text" placeholder="e.g. buy groceries" /> <button onClick={props.addTask}></button>
-    </div>
+    <form onSubmit={addNewTask}>
+      <input
+        id="enter-task" 
+        type="text" 
+        placeholder="e.g. buy groceries" 
+        value={taskName}
+        onChange={changeTask} 
+      />
+      <button type="submit">
+        add
+      </button>
+    </form>
   );
 };
 
