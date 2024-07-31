62cc511460cc8f81c6fbae15d50b57827acf49f0
ctang <11547113+crtang@users.noreply.github.com>
2024-07-31 15:43:41 -0700

    Created Calendar class

---
+++ src/Calendar/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Calendar extends Component {
+  render() {
+    return (
+      <article id="calendar"></article>
+    );
+  };
+};
+
+export default Calendar;
