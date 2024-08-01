c9edc2a6c5868ec2e5abd5632d3214ab56bb3c47
ctang <11547113+crtang@users.noreply.github.com>
2024-08-01 12:40:19 -0700

    Added state for Calendar class and filled in some props and callback methods for CalendarBase

--- src/Calendar/CalendarBase/index.js
+++ src/Calendar/CalendarBase/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 
-const CalendarBase = () => {
+const CalendarBase = (props) => {
+  const prevMonth = (props) => {
+    return props.previous();
+  };
+
+  const nextMonth = (props) => {
+    return props.next();
+  };
+
   return (
     <article id="calendar">
-      <span className="prev">&#10094;</span>
-      <span className="next">&#10095;</span>
+      <span className="prev" onClick={prevMonth}>&#10094;</span>
+      <span className="next" onClick={nextMonth}>&#10095;</span>
 
       <div id="cal-header">
-        <div id="month" className="item"></div>
+        <div id="month" className="item">{props.month} {props.year}</div>
 
         <div class="item wd">sun</div>
         <div class="item wd">mon</div>
--- src/Calendar/index.js
+++ src/Calendar/index.js
@@ -4,7 +4,30 @@ import CalendarBase from './CalendarBase';
 class Calendar extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      today: new Date(),
+      year: today.getFullYear(),
+      month: today.getMonth(),
+    };
   };
+
+  currentMonth = () => {
+    const months = [
+      'january',
+      'februrary',
+      'march',
+      'april',
+      'may',
+      'june',
+      'july',
+      'august',
+      'september',
+      'october',
+      'november',
+      'december'
+    ];
+  }
 
   render() {
     return (
