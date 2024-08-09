e1402ac93b9d9a8e694a81c5ecc17c1b1fc64daa
ctang <11547113+crtang@users.noreply.github.com>
2024-08-09 10:08:17 -0700

    Redoing Calendar component

--- src/Calendar/index.js
+++ src/Calendar/index.js
@@ -1,81 +1,11 @@
 import React, { Component } from 'react';
-import CalendarHeader from './CalendarHeader';
-import CalendarWeeks from './CalendarWeeks';
 
 class Calendar extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      today: new Date(),
-      month: today.getMonth(),
-      year: today.getFullYear(),
-      monthString: '',
-      yearString: ''
-    };
-  };
-
-  currentMonth = () => {
-    const months = [
-      'january',
-      'februrary',
-      'march',
-      'april',
-      'may',
-      'june',
-      'july',
-      'august',
-      'september',
-      'october',
-      'november',
-      'december'
-    ];
-
-    const monthNum = this.state.month;
-    const yearToString = this.state.year.toString();
-
-    this.setState({ monthString: months[monthNum].toString(), yearString: yearToString })
-  };
-
-  previous = () => {
-    // need to temporarily remove .week elements from calendar
-    const monthNow = this.state.month;
-    const yearNow = this.state.year;
-
-    if (this.state.month >= 1) {
-      this.setState({ month: monthNow - 1 })
-    } else if (this.state.month == 0) {
-      this.setState({ month: 11, year: yearNow - 1 });
-    }
-
-    return this.currentMonth();
-  };
-
-  next = () => {
-    // need to temporarily remove .week elements from calendar
-
-    const monthNow = this.state.month;
-    const yearNow = this.state.year;
-
-    if (this.state.month <= 10) {
-      this.setState({ month: monthNow + 1});
-    } else {
-      this.setState({ month: 0, year: yearNow + 1 });
-    }
-
-    return this.currentMonth();
-  };
-
-
-
   render() {
     return (
-      <article id="calendar">
-        <CalendarHeader month={this.state.month} year={this.state.year} />
-        <CalendarWeeks month={this.state.month} year={this.state.year} />
-      </article>  
+      <article id="month-calendar"></article>
     );
   };
-};
+}
 
 export default Calendar;
