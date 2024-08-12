6715dafdb4e0c92c0a81f1cc064a618a1f36e633
ctang <11547113+crtang@users.noreply.github.com>
2024-08-12 10:28:25 -0700

    Wrote methods to get current, previous, and next months

--- src/Calendar/index.js
+++ src/Calendar/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import CalendarHeader from './CalendarHeader';
 
 class Calendar extends Component {
   constructor(props) {
@@ -7,25 +8,59 @@ class Calendar extends Component {
     this.state = {
       today: new Date(),
       month: today.getMonth(),
-      year: today.getFullYear()
+      year: today.getFullYear(),
+      months: [
+        'january',
+        'february',
+        'march',
+        'april',
+        'may',
+        'june',
+        'july',
+        'august',
+        'september',
+        'october',
+        'november',
+        'december'
+      ],
+      m: months[month].toString(),
+      y: year.toString(),
     };
   };
 
   getPreviousMonth = () => {
-    return (
-      this.state.today
-    );
+    const newMonth = 0;
+
+    if (month >= 1) {
+      newMonth = month - 1;
+      this.setState({ month: newMonth });
+    } else if (month == 0) {
+      newMonth = 11;
+      const newYear = year - 1;
+
+      this.setState({ month: newMonth, year: newYear });
+    }
   };
 
   getNextMonth = () => {
-    return (
-      this.state.today
-    );
+    const newMonth = 0;
+
+    if (month < 11) {
+      newMonth = month + 1;
+      this.setState({ month: newMonth });
+    } else if (month == 11) {
+      newMonth = 0;
+      const newYear = year + 1;
+
+      this.setState({ month: newMonth, year: newYear });
+    }
   };
   
   render() {
     return (
-      <article id="month-calendar"></article>
+      <article id="month-calendar">
+        <CalendarHeader />
+      </article>
     );
   };
 }
