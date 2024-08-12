6775c6011e5c67144ff17bada9da8292b4a0cf3f
ctang <11547113+crtang@users.noreply.github.com>
2024-08-12 11:04:53 -0700

    Wrote JSX and functions for calendar header

--- src/Calendar/CalendarHeader/index.js
+++ src/Calendar/CalendarHeader/index.js
@@ -1,8 +1,34 @@
 import React, { useState } from 'react';
 
 const CalendarHeader = (props) => {
+  const displayMonth = m + y;
+
+  const calculatePrevMonth = () => {
+    return props.getPreviousMonth();
+  };
+
+  const calculateNextMonth = () => {
+    return props.getNextMonth();
+  };
+
   return (
-    <div></div>
+    <div id="cal-header">
+      <span id="prev-month" onChange={calculatePrevMonth}>&#10094;</span>
+
+      <span>{ displayMonth }</span>
+
+      <span id="next-month" onChange={calculateNextMonth}>&#10095;</span>
+
+      <div>
+        <div className="item wd">sun</div>
+        <div className="item wd">mon</div>
+        <div className="item wd">tues</div>
+        <div className="item wd">wed</div>
+        <div className="item wd">thurs</div>
+        <div className="item wd">fri</div>
+        <div className="item wd">sat</div>
+      </div>
+    </div>
   );
 };
 
