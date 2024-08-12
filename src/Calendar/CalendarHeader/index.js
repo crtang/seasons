948d36a40d22a6b87dc9c4256e6d080bd5347f68
ctang <11547113+crtang@users.noreply.github.com>
2024-08-12 10:00:37 -0700

    Added CalendarHeader component

---
+++ src/Calendar/CalendarHeader/index.js
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+
+const CalendarHeader = (props) => {
+  return (
+    <div></div>
+  );
+};
+
+export default CalendarHeader;
--- src/Calendar/index.js
+++ src/Calendar/index.js
@@ -1,6 +1,28 @@
 import React, { Component } from 'react';
 
 class Calendar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      today: new Date(),
+      month: today.getMonth(),
+      year: today.getFullYear()
+    };
+  };
+
+  getPreviousMonth = () => {
+    return (
+      this.state.today
+    );
+  };
+
+  getNextMonth = () => {
+    return (
+      this.state.today
+    );
+  };
+  
   render() {
     return (
       <article id="month-calendar"></article>
